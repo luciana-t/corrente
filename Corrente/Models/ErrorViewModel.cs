54a412d86b8a21595a219e79595120464824fb00
Luciana Tavares <lucianatab@gmail.com>
2021-03-14 10:14:21 -0300

    Project created

---
+++ Corrente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Corrente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
