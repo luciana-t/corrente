979c03eb02348fa97c05d4a0da502e13839fa983
Luciana Tavares <lucianatab@gmail.com>
2021-03-14 16:44:47 -0300

    CRUD Scaffolding

---
+++ Corrente/Data/CorrenteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Corrente.Models;
+
+namespace Corrente.Data
+{
+    public class CorrenteContext : DbContext
+    {
+        public CorrenteContext (DbContextOptions<CorrenteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Corrente.Models.TipoInstituicao> TipoInstituicao { get; set; }
+    }
+}
--- Corrente/Startup.cs
+++ Corrente/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Corrente.Data;
 
 namespace Corrente
 {
@@ -33,6 +35,9 @@ namespace Corrente
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CorrenteContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CorrenteContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
