d31fe952058486a367986493d49ab9a96d221efd
Luciana Tavares <lucianatab@gmail.com>
2021-03-29 20:39:29 -0300

    InstituicaoFormViewModel and TipoInstituicao select component

---
+++ Corrente/Services/TipoInstituicaoService.cs
@@ -0,0 +1,23 @@
+﻿using Corrente.Data;
+using Corrente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Corrente.Services
+{
+    public class TipoInstituicaoService
+    {
+        private readonly CorrenteContext _context;
+
+        public TipoInstituicaoService(CorrenteContext context)
+        {
+            _context = context;
+        }
+        
+        public List<TipoInstituicao> FindAll()
+        {
+            return _context.TipoInstituicao.OrderBy(x => x.Name).ToList();        }
+    }
+}
